73dadef48b5909f62a1e7668f614bd1e3086b7bb
ZenBogdan2604 <93253276+ZenBogdan2604@users.noreply.github.com>
2023-10-24 13:23:58 +0300

    add photo from another file .vue cli

---
+++ src/components/PictureComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <img src="./assets/USSR.png" alt="Имба">
+    <img src="./assets/ИС-7.jpg" alt="Под пиво">
+    <img src="./assets/Isu152-1.jpg" alt="Фугасы онли">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PictureComponent',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+      <h1>Кого стоит качать в танках?</h1>
+  <hr>
+   <ul>
+     <li>Совок</li>
+     <li>ИС-7</li>
+     <li>Ису-152(Бабаха)</li>
+   </ul>
+   <hr>
+   <PictureComponent/>
+    </div>
+  </template>
+  
+  <script>
+
+import PictureComponent from './PictureComponent.vue';
+
+  export default {
+    name: 'TestComponent',
+    components: {
+     PictureComponent
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  </style>
